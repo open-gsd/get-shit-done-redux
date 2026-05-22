4761dde7718e7ee197ced3ae874ef3e2c709ffa6
Tom Boucher <trekkie@nomorestars.com>
2026-05-22 12:39:05 -0400

    fix(tests): escape '/' in forensics test regex literals
    Closes #3855

--- tests/forensics.test.cjs
+++ tests/forensics.test.cjs
@@ -150,7 +150,7 @@ describe('forensics workflow', () => {
     // contains the repo string.
     assert.match(
       content,
-      /gh issue create[\s\S]{0,250}--repo\s+open-gsd/get-shit-done-redux/,
+      /gh issue create[\s\S]{0,250}--repo\s+open-gsd\/get-shit-done-redux/,
       'gh issue create must use --repo open-gsd/get-shit-done-redux to avoid submitting to the user\'s current project repo'
     );
   });
@@ -160,7 +160,7 @@ describe('forensics workflow', () => {
     // Regex is more robust than a fixed-length slice to formatting changes
     assert.match(
       content,
-      /gh label list[\s\S]{0,250}--repo\s+open-gsd/get-shit-done-redux/,
+      /gh label list[\s\S]{0,250}--repo\s+open-gsd\/get-shit-done-redux/,
       'gh label list must target open-gsd/get-shit-done-redux'
     );
   });
