gsd-build/get-shit-done

fix(3668): adopt inline-per-fence SDK resolution + restore safety semantics
## Architectural choice (inline-per-fence, option a)

The brief offered three options:
  (a) inline preflight block per fence
  (b) wrapper script
  (c) shared shell fragment sourced at the top

71 of 72 workflow files already had inline preflight blocks (just broken ones).
Option (b)/(c) would have required changes to install.js + a new shared artifact,
with significant risk of breaking the install pipeline. Option (a) was the path
of least resistance and least new blast radius.

## What this commit fixes

**BLOCKER 1+2+3 (quick.md — GSD_SDK never assigned):**
- quick.md had 12 `$GSD_SDK` references but zero `GSD_SDK=` assignments.
- Added proper local-first preflight block with `git rev-parse --show-toplevel`
  path (not the broken `CLAUDE_FILE_PATHS` which is always empty in Claude Code).
- Each Bash fence in Claude Code runs as a fresh `bash -c`, so env vars don't
  persist. The preflight block must appear in every fence that uses $GSD_SDK.

**BLOCKER 4 (execute-phase.md — || exit 1 dropped):**
- Restored `|| exit 1` after every `worktree.cleanup-wave` call. SDK safety
  refusals (drift detection #3174, deletion block #2384) must surface, not be
  swallowed by the old `|| { fallback }` branch.

**F5 (verify-work.md untyped fence):**
- Changed bare `gsd-sdk` in an untyped fence to `$GSD_SDK`.
- Changed fence tag from untyped to `bash`.

**F6 (non-recursive readdirSync):**
- Defect 2 test now uses `findMdFiles` (recursive) to cover workflow
  subdirectories, not the flat `fs.readdirSync` that missed subdirs.

**F7 (lint misses untyped fences):**
- `parseMarkdownSegments` now treats `lang === ''` fences as bash-fences.

**F8 (missing propagation test):**
- Added two propagation tests in the Defect 3 describe block.

**F9 (priority inverted — global before local):**
- All 72 workflow files now check `[ -f "$GSD_TOOLS" ]` before `command -v gsd-sdk`.
- Path: `$(git rev-parse --show-toplevel 2

tests/bug-3091-sdk-package-guidance-and-fallbacks.test.cjs
@@ -14,7 +14,11 @@ function read(rel) {
 describe('bug #3091: sdk install guidance and agent fallbacks use query-capable CLI', () => {
   test('quick workflow install hint references get-shit-done-cc (not @gsd-build/sdk)', () => {
     const content = read('get-shit-done/workflows/quick.md');
-    assert.ok(content.includes('npm install -g get-shit-done-cc'));
+    // After #3797: quick.md uses the standard local-first preflight which references
+    // get-shit-done-cc via npx (not `npm install -g`). Either form is acceptable.
+    const referencesGsdCc = content.includes('npm install -g get-shit-done-cc') ||
+      content.includes('npx get-shit-done-cc');
+    assert.ok(referencesGsdCc, 'quick.md install hint must reference get-shit-done-cc');
     assert.ok(!content.includes('npm install -g @gsd-build/sdk'));
   });
 

open-gsd/get-shit-done-redux

tests/bug-3091-sdk-package-guidance-and-fallbacks.test.cjs
@@ -14,8 +14,13 @@ function read(rel) {
 describe('bug #3091: sdk install guidance and agent fallbacks use query-capable CLI', () => {
   test('quick workflow install hint references get-shit-done-redux (not @gsd-redux/sdk)', () => {
     const content = read('get-shit-done/workflows/quick.md');
-    assert.ok(content.includes('npm install -g get-shit-done-redux'));
+    // After #3668: quick.md uses local-first preflight which may reference
+    // get-shit-done-redux via npx or npm install -g. Either form is acceptable.
+    const referencesGsdRedux = content.includes('npm install -g get-shit-done-redux') ||
+      content.includes('npx get-shit-done-redux');
+    assert.ok(referencesGsdRedux, 'quick.md install hint must reference get-shit-done-redux');
     assert.ok(!content.includes('npm install -g @gsd-redux/sdk'));
+    assert.ok(!content.includes('npm install -g @gsd-build/sdk'));
   });
 
   test('agent docs no longer reference node_modules/@gsd-redux/sdk/dist/cli.js query fallback', () => {